e25516d076cac5e51c07a67c74897a08015a0fa6
oliver staudt <trollabeissa@gmail.com>
2021-09-25 12:38:40 +0200

    authorization added

--- devicelist.sh
+++ devicelist.sh
@@ -3,3 +3,5 @@ source ./config.sh
 echo FREEATHOME_HOST: $FREEATHOME_HOST
 echo FREEATHOME_USR:  $FREEATHOME_USR
 echo FREEATHOME_PWD:  $FREEATHOME_PWD
+
+curl -X GET "http://$FREEATHOME_USR:$FREEATHOME_PWD@$FREEATHOME_HOST/fhapi/v1/api/rest/devicelist" -H  "accept: application/json"
